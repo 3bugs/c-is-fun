f9f97f51e5241c2227e0fa3733ed149f2e82368b
Promlert Lovichit <promlert@gmail.com>
2020-02-05 15:00:47 +0700

    Add test_fp.c

---
+++ test_fp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("0.1 + 0.2 = %.10f\n\n", 0.1f + 0.2f);
+
+    printf("0.1 is represented as %.10f\n", 0.1f);
+    printf("0.2 is represented as %.10f\n\n", 0.2f);
+    
+    return 0;
+}
